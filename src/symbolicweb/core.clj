5261e021c226218157fdedef08de9915b16c5f7d
Lars Rune Nøstdal <larsnostdal@gmail.com>
2011-07-19 22:46:35 +0200

    HANDLER: This doesn't belong here; in general an URL-based dispatcher has not been implemented yet.

--- src/symbolicweb/core.clj
+++ src/symbolicweb/core.clj
@@ -86,44 +86,38 @@
                   *viewport* viewport]
           (if-let [sw-request-type (get (:query-params req) "_sw_request-type")]
             (handle-sw-request-type sw-request-type) ;; It's an AJAX or Comet request.
-            ;;(handle-normal-request-type) ;; It's a "normal" request.
-            ;; TODO: In production, this should check host name instead of URL.
-            (if (= (subs (:uri req) 0 (min 13 (count (:uri req))))
-                   "/sw/alfatekst")
-              (handler-alfatekst req)
+            {:status  200
+             :headers {"Content-Type" "text/html; charset=UTF-8"
+                       "Connection" "keep-alive"
+                       "Expires" "Mon, 26 Jul 1997 05:00:00 GMT"
+                       "Cache-Control" "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
+                       "Pragma" "no-cache"}
+             :body
+             (html
+              (doctype :xhtml-strict)
+              (xhtml-tag
+               "en"
+               [:head
+                [:title "SymbolicWeb"]
+                [:meta {:http-equiv "Content-Type" :content "text/html; charset=UTF-8"}]
+                (script-src "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js")
+                (sw-js-bootstrap)]
 
-              {:status  200
-               :headers {"Content-Type" "text/html; charset=UTF-8"
-                         "Connection" "keep-alive"
-                         "Expires" "Mon, 26 Jul 1997 05:00:00 GMT"
-                         "Cache-Control" "no-store, no-cache, must-revalidate, post-check=0, pre-check=0"
-                         "Pragma" "no-cache"}
-               :body
-               (html
-                (doctype :xhtml-strict)
-                (xhtml-tag
-                 "en"
-                 [:head
-                  [:title "SymbolicWeb"]
-                  [:meta {:http-equiv "Content-Type" :content "text/html; charset=UTF-8"}]
-                  (script-src "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js")
-                  (sw-js-bootstrap)]
+               [:body
+                [:h1 "SymbolicWeb - bringing life to the web!"]
+                [:p "Hello, this is SymbolicWeb running on Clojure " (clojure-version)]
+                [:p "Norwegian characters: æøå."]
+                [:ul (for [i (range 10)]
+                       [:li [:b "This is nr. " i "."]])]
 
-                 [:body
-                  [:h1 "SymbolicWeb - bringing life to the web!"]
-                  [:p "Hello, this is SymbolicWeb running on Clojure " (clojure-version)]
-                  [:p "Norwegian characters: æøå."]
-                  [:ul (for [i (range 10)]
-                         [:li [:b "This is nr. " i "."]])]
+                [:p "Here is the Clojure source code for this page:"
+                 [:pre (slurp "src/symbolicweb/core.clj")]]
 
-                  [:p "Here is the Clojure source code for this page:"
-                   [:pre (slurp "src/symbolicweb/core.clj")]]
-
-                  [:p (link-to "http://validator.w3.org/check?uri=referer"
-                               [:img {:src "http://www.w3.org/Icons/valid-xhtml10"
-                                      :alt "Valid XHTML 1.0 Strict"
-                                      :height 31
-                                      :width  88}])]]))})))))))
+                [:p (link-to "http://validator.w3.org/check?uri=referer"
+                             [:img {:src "http://www.w3.org/Icons/valid-xhtml10"
+                                    :alt "Valid XHTML 1.0 Strict"
+                                    :height 31
+                                    :width  88}])]]))}))))))
 
 
 (defn -main [& args]
