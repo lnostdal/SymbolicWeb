d91f3b5d539e11521fe714c755c5823d8fdf57b2
Lars Rune Nøstdal <larsnostdal@gmail.com>
2011-07-21 17:13:51 +0200

    Move globals from common.clj to globals.clj. This will make it easier to eval the entire common.clj file in one go without also overwriting global variables, which is often not the intent.

---
+++ src/symbolicweb/globals.clj
@@ -0,0 +1,56 @@
+(in-ns 'symbolicweb.core)
+
+(def -comet-timeout- 29000)
+(def -viewport-timeout- (+ -comet-timeout- 30000))
+(def -application-timeout- (+ -viewport-timeout- 30000))
+
+
+(def -application-types-
+  "name -> {:fit-fn fit-fn
+            :application-constructor-fn application-constructor-fn]"
+  (atom {}))
+
+(def -applications-
+  "ID -> APPLICATION"
+  (atom {}))
+
+(def -viewports-
+  "ID -> VIEWPORT"
+  (atom {}))
+
+(def -gc-thread-
+  "This thing iterates through all sessions in -APPLICATIONS- and -VIEWPORTS- end checks their :LAST-ACTIVITY-TIME properties
+removing unused or timed out sessions."
+  (agent 42))
+
+(send-off -gc-thread-
+          (fn [_]
+            (loop  []
+              (let [now (System/currentTimeMillis)
+                    checker-fn (fn [cnt timeout]
+                                 (doseq [obj @cnt]
+                                   (let [obj @(val obj)]
+                                     (when (< timeout (- now (:last-activity-time obj)))
+                                       (swap! cnt #(dissoc % (:id obj)))))))]
+                (checker-fn -applications- -application-timeout-)
+                (checker-fn -viewports- -viewport-timeout-)
+                (when (= *agent* -gc-thread-)
+                  (Thread/sleep 5000)
+                  (recur))))))
+
+(def -widgets-
+  "ID -> WIDGET"
+  (atom {}))
+
+
+(def ^:dynamic *request*
+  "The current request data (map)."
+  nil)
+
+(def ^:dynamic *viewport*
+  "The current VIEWPORT."
+  nil)
+
+(def ^:dynamic *application*
+  "The current APPLICATION (session)."
+  nil)
