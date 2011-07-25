00f4e1ad3b33ad3d89711baac1d06c3cce0ffa37
Lars Rune Nøstdal <larsnostdal@gmail.com>
2011-07-25 08:29:46 +0200

    Remove the dependency on java.jdbc.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
                  [hiccup "0.3.6"]
                  [enlive "1.2.0-alpha1"]
-                 [ring "0.3.11"]
-                 [org.clojure/java.jdbc "0.0.5-SNAPSHOT"]]
+                 [ring "0.3.11"]]
 
   :main symbolicweb.core)
