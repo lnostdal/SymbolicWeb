b31e2a05a8f5662a8b9e77308ca47e68e2e98cf2
Lars Rune Nøstdal <lnostdal@blackbox>
2011-10-13 13:54:01 +0200

    Dialog: Handle basic options.

--- src/symbolicweb/dialog.clj
+++ src/symbolicweb/dialog.clj
@@ -1,11 +1,25 @@
 (in-ns 'symbolicweb.core)
 
-(derive ::Dialog ::HTMLElement)
-(defn make-Dialog [widget]
-  "\"Convert\" WIDGET into a jQuery UI Dialog."
-  (with1 widget
-    (add-response-chunk (str "$('#" (:id @it) "')"
-                             ".dialog({modal: true, close: function(event, ui){"
-                             "  $('#" (:id @it) "').remove();"
-                             "}});")
-                        it)))
+
+(letfn [(handle-js-options [opts]
+          (with-out-str
+            (doseq [opt opts]
+              (print (str (name (key opt)) ": "
+                          (with (val opt)
+                            (if (keyword? it)
+                              (name it)
+                              it))
+                          ", ")))))]
+
+  (defn make-Dialog
+    "\"Convert\" WIDGET into a jQuery UI Dialog.
+DIALOG-JS-OPTIONS can be e.g. {:width 800 :modal true} etc., see the jQuery UI Dialog docs."
+    ([widget] (make-Dialog widget {}))
+    ([widget dialog-js-options]
+       (with1 widget
+         (add-response-chunk (str "$('#" (:id @it) "')"
+                                  ".dialog({" (handle-js-options dialog-js-options)
+                                  "close: function(event, ui){"
+                                  "  $('#" (:id @it) "').remove();"
+                                  "}});")
+                             it)))))
