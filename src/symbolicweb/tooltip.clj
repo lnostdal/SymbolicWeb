0a8bd99a7ca8136ea20a980d7b8696cf7993b149
Lars Rune Nøstdal <lnostdal@blackbox>
2011-10-19 14:07:33 +0200

    ErrorTooltip: Added. Also, some JS option handling added to both ErrorTooltip and Tooltip.

--- src/symbolicweb/tooltip.clj
+++ src/symbolicweb/tooltip.clj
@@ -7,15 +7,23 @@
                       context-widget))
 
 
-(defn show-Tooltip [context-widget message & options]
+(defn show-Tooltip
   "Show Tooltip for CONTEXT-WIDGET. MESSAGE is the string to show in Tooltip message."
-  (add-response-chunk (str "$('#" (:id @context-widget) "')"
-                           ".attr('title', '')"
-                           ".tooltip({"
-                           "  content: function(){ return " (url-encode-wrap message) "}"
-                           "}).tooltip('open');")
-                      context-widget))
+  ([context-widget message]
+     (show-Tooltip context-widget message nil))
+  ([context-widget message options]
+     (add-response-chunk (str "$('#" (:id @context-widget) "')"
+                              ".attr('title', '')"
+                              ".tooltip({"
+                              (map-to-js-options options)
+                              "  content: function(){ return " (url-encode-wrap message) "}"
+                              "}).tooltip('open');")
+                         context-widget)))
 
 
-(defn show-ErrorTooltip [widget & options]
-  )
+(defn show-ErrorTooltip
+  "Show Tooltip for CONTEXT-WIDGET. MESSAGE is the string to show in Tooltip message."
+  ([context-widget message]
+     (show-ErrorTooltip context-widget message {}))
+  ([context-widget message options]
+     (show-Tooltip context-widget message (conj options [:tooltipClass "'ui-state-error'"]))))
