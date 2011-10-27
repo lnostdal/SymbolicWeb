5499f3f68f10ff7640a0ef665e3ebf9247c3a267
Lars Rune Nøstdal <lnostdal@blackbox>
2011-10-27 20:34:15 +0200

    make-HashedTextInput renamed to make-HashedInput. Also, support for salting of the hash added.

--- src/symbolicweb/text_input.clj
+++ src/symbolicweb/text_input.clj
@@ -11,32 +11,38 @@
 
 (derive ::TextInput ::HTMLElement)
 (defn make-TextInput [model & attributes]
+  "<input type='text' ..> type widget."
   (with1 (apply make-HTMLElement "input" model
                 :type ::TextInput
                 :static-attributes {:type "text"}
                 :handle-model-event-fn (fn [widget _ new-value]
                                          (jqVal widget new-value))
                 attributes)
-    (set-event-handler "change" it
-                       (fn [& {:keys [new-value]}]
-                         (set-value model (if-let [input-parsing-fn (:input-parsing-fn @it)]
-                                            (input-parsing-fn new-value)
-                                            new-value)))
-                       :callback-data {:new-value "' + encodeURIComponent($(this).val()) + '"})))
+         (set-event-handler "change" it
+                            (fn [& {:keys [new-value]}]
+                              (set-value model (if-let [input-parsing-fn (:input-parsing-fn @it)]
+                                                 (input-parsing-fn new-value)
+                                                 new-value)))
+                            :callback-data {:new-value "' + encodeURIComponent($(this).val()) + '"})))
 
 
-(derive ::HashedTextInput ::HTMLElement)
-(defn make-HashedTextInput [model & attributes]
-  (with1 (apply make-TextInput model
-                :handle-model-event-fn (fn [_ _ _])
+(derive ::HashedInput ::HTMLElement)
+(defn make-HashedInput [model salt & attributes]
+  "<input type='password' ..> type widget using salted SHA256 hashing on the client end.
+Note that the salted hash is still transferred (client -> server) in clear text form."
+  (with1 (apply make-HTMLElement "input" model
+                :type ::TextInput
                 :static-attributes {:type "password"}
+                :handle-model-event-fn (fn [_ _ _])
                 attributes)
-    (set-event-handler "change" it
-                       (fn [& {:keys [new-value]}]
-                         (set-value model (if-let [input-parsing-fn (:input-parsing-fn @it)]
-                                            (input-parsing-fn new-value)
-                                            new-value)))
-                       :callback-data {:new-value "' + encodeURIComponent($.sha256($(this).val())) + '"})))
+         (set-event-handler "change" it
+                            (fn [& {:keys [new-value]}]
+                              (set-value model (if-let [input-parsing-fn (:input-parsing-fn @it)]
+                                                 (input-parsing-fn new-value)
+                                                 new-value)))
+                            :callback-data
+                            {:new-value "' + encodeURIComponent($.sha256(decodeURIComponent('"
+                             (url-encode (or salt "")) "') + $(this).val())) + '"})))
 
 
 (derive ::IntInput ::TextInput)
