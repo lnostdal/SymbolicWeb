c0d0db554c0b95fd2f4b2bc4727ee207457f96bd
Lars Rune Nøstdal <lnostdal@blackbox>
2011-12-03 05:20:25 +0100

    make-Dialog: Handle WITH-JS corretly. show-Dialog: Return the created Dialog; not the widget it wraps. show-ModalDialog: Return the created Dialog; not the widget it wraps. close-Dialog: Add TODO concerning leak.

--- src/symbolicweb/dialog.clj
+++ src/symbolicweb/dialog.clj
@@ -13,33 +13,38 @@ DIALOG-JS-OPTIONS can be e.g. {:width 800 :modal true} etc., see the jQuery UI D
        (make-Dialog (mk-p (vm \"test\"))
                     :js-options {:modal :true :width 800 :height 600}
                     :on-close (with-js (alert \"Dialog was closed.\")))))"
-  (with1 widget
-    (add-response-chunk (str "$('#" (:id @it) "')"
-                             ".dialog({"
-                             (map-to-js-options js-options)
-                             "close: function(event, ui){"
-                             "  $('#" (:id @it) "').remove();"
-                             on-close
-                             "}});")
-                        it)))
+  (let [res (add-response-chunk (str "$('#" (:id @widget) "')"
+                                     ".dialog({"
+                                     (map-to-js-options js-options)
+                                     "close: function(event, ui){"
+                                     "  $('#" (:id @widget) "').remove();"
+                                     on-close
+                                     "}});")
+                                widget)]
+    (if *with-js?*
+      res
+      widget)))
+
+
 
 
 ;;(with (show-Dialog (whc ["div"] [:p "Hello World"])
 ;;                   :js-options {:buttons "[{ text: \"Ok\", click: function(){ $(this).dialog(\"close\"); }}]"})
 ;;  (set-event-handler "dialogclose" it (fn [] (println "dialogclose"))))
 
+
 (defn show-Dialog [widget & options]
-  (with1 widget
-    (jqAppend (root-element)
-      (apply make-Dialog it options))))
+  ;; TODO: Yeah, (ROOT-ELEMENT) doesn't generally mean the same thing anymore when the root can be the entire document.
+  (jqAppend (root-element) ;;(:main-content-view @*viewport*)
+    (apply make-Dialog widget options)))
 
 
 (defn show-ModalDialog [widget & options]
-  (with1 widget
-    (apply show-Dialog it
-           (alter-options options update-in [:js-options] assoc :modal :true))))
+  (apply show-Dialog widget
+         (alter-options options update-in [:js-options] assoc :modal :true)))
 
 
 (defn close-Dialog [widget]
+  ;; TODO: But this never removes it on the server end; i.e. jqAppend also accumulates on the server end.
   (add-response-chunk (str "$('#" (:id @widget) "').dialog('close');")
                       widget))
