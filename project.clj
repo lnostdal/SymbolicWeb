b7c114203ec9c834cf3f5b726e69f813e686a2d3
Lars Rune Nøstdal <lnostdal@blackbox>
2011-12-20 16:46:03 +0100

    Cleanup project.clj a bit.

--- project.clj
+++ project.clj
@@ -1,24 +1,21 @@
 (defproject symbolicweb "1.0.0-SNAPSHOT"
   :description "SymbolicWeb: WUI for Clojure"
   :dependencies [[org.clojure/clojure "1.4.0-master-SNAPSHOT"]
-                 [org.clojure/math.numeric-tower "0.0.2-SNAPSHOT"]
 
-                 ;; Yay, EPOLL() based handling of HTTP.
-                 [aleph "0.2.1-SNAPSHOT"]
-                 [overtone/at-at "0.2.1"]
+                 [org.clojure/math.numeric-tower "0.0.2-SNAPSHOT"] ;; ROUND etc.
 
-                 ;;[clache "0.7.0"] ;; Caching.
-                 ;;[unk "0.9.3"]
-                 [org.apache.commons/collections "3.2.1"]
+                 [aleph "0.2.1-SNAPSHOT"] ;; Yay, EPOLL() based handling of HTTP.
+                 [overtone/at-at "0.2.1"] ;; Scheduling.
 
-                 ;; JSON.
-                 [cheshire "2.0.5-SNAPSHOT"]
+                 [org.apache.commons/collections "3.2.1"] ;; For soft hash cache.
 
-                 [hiccup "0.3.7"]
-                 [enlive "1.2.0-alpha1"]
-                 [ring/ring-core "1.0.0-RC4"]
+                 [cheshire "2.0.5-SNAPSHOT"] ;; JSON.
 
-                 [org.clojure/java.jdbc "0.1.2-SNAPSHOT"]
-                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [hiccup "0.3.7"] ;; HTML generation.
+                 [enlive "1.2.0-alpha1"] ;; HTML templating.
+                 [ring/ring-core "1.0.1"] ;; HTTP handling.
+
+                 [org.clojure/java.jdbc "0.1.2-SNAPSHOT"] ;; SQL/DB handling.
+                 [postgresql/postgresql "9.1-901.jdbc4"] ;; PostgreSQL JDBC driver.
                  [c3p0/c3p0 "0.9.1.2"]] ;; DB connection pooling.
   :jvm-opts ["-server" "-XX:+TieredCompilation"])
