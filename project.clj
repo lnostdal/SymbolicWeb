223423ac4fa695ac23e51dc3929d3100171393b9
Lars Rune Nøstdal <larsnostdal@gmail.com>
2012-02-09 01:03:30 +0100

    ...

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
                  [org.clojure/math.numeric-tower "0.0.2-SNAPSHOT"] ;; ROUND etc.
 
-                 [aleph "0.2.1-alpha2-SNAPSHOT"] ;; Yay, EPOLL() based handling of HTTP.
+                 [aleph "0.2.1-alpha2-SNAPSHOT"] ;; Deals with boring HTTP server stuff.
                  [overtone/at-at "0.2.1"] ;; Scheduling.
 
                  [org.apache.commons/collections "3.2.1"] ;; For soft hash cache.
@@ -13,10 +13,11 @@
 
                  [hiccup "0.3.8"] ;; HTML generation.
                  [enlive "1.2.0-alpha1"] ;; HTML templating.
-                 [ring/ring-core "1.0.1"] ;; HTTP handling.
+                 [ring/ring-core "1.0.1"] ;; HTTP protocol handling.
 
                  [org.clojure/java.jdbc "0.1.2-SNAPSHOT"] ;; SQL/DB handling.
                  [postgresql/postgresql "9.1-901.jdbc4"] ;; PostgreSQL JDBC driver.
                  [c3p0/c3p0 "0.9.1.2"]] ;; DB connection pooling.
-  :jvm-opts ["-server" "-XX:+TieredCompilation"
+  :jvm-opts [;;"-Xdebug" "-Xrunjdwp:transport=dt_socket,server=y,suspend=n" ;; For JSwat.
+             "-server" "-XX:+TieredCompilation"
              "-XX:-OmitStackTraceInFastThrow"]) ;; http://stackoverflow.com/a/2070568/160305
