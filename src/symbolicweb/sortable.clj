d8882ca72f603a876dfa1ee85ef3d933d2ef47c8
Lars Rune Nøstdal <larsnostdal@gmail.com>
2012-03-16 06:14:33 +0100

    Sortable: WIP; I've made a temp improvement by reversing the order in which the redraw ("resort") is done, but this thing is still very slow.

--- src/symbolicweb/sortable.clj
+++ src/symbolicweb/sortable.clj
@@ -7,23 +7,29 @@
    "sortupdate" widget
    (fn [& {:keys [new-order]}]
      (dosync
-     (let [new-order-ids (json/decode new-order)
-           new-order-nodes (with-local-vars [nodes []]
-                             (doseq [widget-id new-order-ids]
-                               (var-set nodes (conj (var-get nodes) (dosync (:node-of-view @(get-widget widget-id))))))
-                             (var-get nodes))
-           container-model (container-model (:node-of-view @(get-widget (first new-order-ids))))
-           existing-order-nodes (with-local-vars [nodes []]
-                                  (loop [node (cm-head-node container-model)]
-                                    (var-set nodes (conj (var-get nodes) node))
-                                    (when-let [right-node (cmn-right-node node)]
-                                      (recur right-node)))
-                                  (var-get nodes))]
-       ;; TODO: Quite crude redraw.
-       ;; This also fucks up stuff that still thinks the old nodes are what holds our current view of auctions.
-       (doseq [node new-order-nodes]
-         (cm-append container-model (cmn (node-data node))))
-       (doseq [node existing-order-nodes]
-         (cmn-remove node))
-       (when callback (callback new-order-nodes)))))
+      (println "sortable: retry?")
+      (let [new-order-ids (json/decode new-order)
+            new-order-nodes (with-local-vars [nodes []]
+                              (doseq [widget-id new-order-ids]
+                                (var-set nodes (conj (var-get nodes) (:node-of-view @(get-widget widget-id)))))
+                              (var-get nodes))
+            container-model (do
+                              ;;(container-model (:node-of-view @(get-widget (first new-order-ids))))
+                              (:model @widget))
+            existing-order-nodes (with-local-vars [nodes []]
+                                   (loop [node (cm-head-node container-model)]
+                                     (var-set nodes (conj (var-get nodes) node))
+                                     (when-let [right-node (cmn-right-node node)]
+                                       (recur right-node)))
+                                   (var-get nodes))]
+        ;; TODO: Quite crude redraw.
+        ;; This also fucks up stuff that still thinks the old nodes are what holds our current view of auctions.
+        (doseq [node existing-order-nodes]
+          (cmn-remove node))
+        (time
+         (doseq [node new-order-nodes]
+           (cm-append container-model (cmn (node-data node)))))
+
+        (when callback (callback new-order-nodes))))
+     (println "sortable: done!"))
    :callback-data {:new-order (str "' + encodeURIComponent(JSON.stringify($('#" (:id @widget) "').sortable('toArray'))) + '")}))
