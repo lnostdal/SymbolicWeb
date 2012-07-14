6450b9404b61284b5ec4a53e55ccd5767d002ac2
Lars Rune Nøstdal <larsnostdal@gmail.com>
2012-07-14 19:51:55 +0200

    MailChimp: STS --> Mandrill.

--- src/symbolicweb/mail_chimp.clj
+++ src/symbolicweb/mail_chimp.clj
@@ -23,55 +23,39 @@
 
 
 (defn make-MailChimp [api-key]
-  (with {:api-key api-key
-         :subdomain (second (str/split api-key #"-"))}
-    (assoc it
-      :url (str "https://" (:subdomain it) ".sts.mailchimp.com/1.0/"))))
+  {:api-key api-key
+   :url "https://mandrillapp.com/api/1.0/"})
+
+
+;;;; Mail Chimp: Mandrill
+;;;; http://mandrillapp.com/api/docs/index.html
+
+;;; Users Calls
+
+(defn users-info [context]
+  (http-post-request (str (:url context) "users/info.json")
+                     (json-generate {:key (:api-key context)})))
+
+(defn users-ping [context]
+  (http-post-request (str (:url context) "users/ping.json")
+                     (json-generate {:key (:api-key context)})))
+
+(defn users-senders [context]
+  (http-post-request (str (:url context) "users/senders.json")
+                     (json-generate {:key (:api-key context)})))
+
+
+;;; Messages Calls
+
+(defn messages-send [context message]
+  (http-post-request (str (:url context) "messages/send.json")
+                     (json-generate {:key (:api-key context)
+                                     :message message})))
 
 
 
-
-;;; Mail Chimp STS API.
-;;;   http://apidocs.mailchimp.com/sts/
-
-(defn send-email [context from-name from-email to-name-email-map subject text html & args]
-  (let [params (apply assoc {:message {:from_name (mime-encode-rfc-2047 from-name)
-                                       :from_email from-email
-                                       :to_email (into [] (vals to-name-email-map))
-                                       :to_name (mapv mime-encode-rfc-2047 (keys to-name-email-map))
-                                       :subject subject
-                                       :text text
-                                       :html html}}
-                      :apikey (:api-key context)
-                      args)]
-    ;; TODO: I have no idea why Mail Chimp doesn't expect all the arguments to go in the body? This will surely break ref. URL
-    ;; length limits?
-    (http-post-request (str (:url context) "SendEmail?" (subs (http-build-query params) 1))
-                       "")))
-
-
-
-
-
-#_(defn mail-chimp-test []
-  (let [to_emails ["larsnostdal@gmail.com"
-                   ;;"paalkiil@gmail.com"
-                   ]
-        to_names ["Lars Rune Nøstdal: ÆØå æøå ☺"
-                  ;;"Pål Kiil: ÆØÅ æøå ☺"
-                  ]
-        message {;;:html "Dette er <b>HTML-koden</b>. Fungerer Norske tegn? ÆØÅ æøå ☺"
-                 :text "Dette er *teksten*. Fungerer Norske tegn? ÆØÅ æøå ☺"
-                 :subject "Free or Deal er ikke spam: ÆØÅ æøå ☺"
-                 :from_name (mime-encode-rfc-2047 "Free or Deal: ÆØÅ æøå ☺")
-                 :from_email "larsnostdal@gmail.com"
-                 :to_email to_emails
-                 :to_name (mapv mime-encode-rfc-2047 to_names)}
-        tags ["WelcomeEmail"]
-        params {:message message
-                :track_opens false ;;true
-                :track_clicks false
-                :tags tags}]
-    ;;(send-email (make-MailChimp nil nil) params)
-    (url-decode-component (http-build-query params)))
-  )
+#_(messages-send (make-MailChimp (:mandrill_key FreeOrDeal.core/-config-data-))
+               {:subject "test subject"
+                :from_email "larsnostdal@gmail.com"
+                :to [{:email "larsnostdal@gmail.com"}]
+                :html "blah"})
