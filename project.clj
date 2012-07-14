64aad7e3425d1025249c17c695965ea6152cd102
Lars Rune Nøstdal <larsnostdal@gmail.com>
2012-07-14 19:52:25 +0200

    New deps .. aleph etc..

--- project.clj
+++ project.clj
@@ -4,18 +4,19 @@
 
                  [org.clojure/math.numeric-tower "0.0.2-SNAPSHOT"] ;; ROUND etc.
 
-                 [aleph "0.2.1"] ;; Deals with boring HTTP server stuff.
+                 [aleph "0.3.0-SNAPSHOT"] ;; Deals with boring HTTP server stuff.
+
                  [overtone/at-at "1.0.0"] ;; Scheduling.
 
                  [org.apache.commons/collections "3.2.1"] ;; For soft hash cache.
 
-                 [cheshire "4.0.1-SNAPSHOT"] ;; JSON.
+                 [cheshire "4.0.2-SNAPSHOT"] ;; JSON.
 
                  [hiccup "1.0.0"] ;; HTML generation.
                  [org.jsoup/jsoup "1.6.4-SNAPSHOT"] ;; HTML templating.
                  [ring/ring-core "1.1.0"] ;; HTTP protocol handling.
 
-                 [org.clojure/java.jdbc "0.2.2-SNAPSHOT"] ;; SQL/DB handling.
+                 [org.clojure/java.jdbc "0.2.4-SNAPSHOT"] ;; SQL/DB handling.
                  [postgresql/postgresql "9.1-902.jdbc4"] ;; PostgreSQL JDBC driver.
                  [c3p0/c3p0 "0.9.1.2"]] ;; DB connection pooling.
   :plugins [[swank-clojure "1.5.0-SNAPSHOT"]]
