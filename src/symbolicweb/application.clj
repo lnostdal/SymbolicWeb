lnostdal/SymbolicWeb

commit 8f7db2c9eb83bbf223cb18b16f7c90d991535812

WIP issue #205 in FoD.

src/symbolicweb/application.clj
@@ -76,7 +76,11 @@ On page load (or refresh), the order of things executed are:
              (make-Application :rest-handler #'clear-session-page-handler :session? false))
            ;; Session cookie not sent; the user is requesting a brand new session or Application.
            (if-let [application-constructor (find-application-constructor request)]
-             (application-constructor)
+             (application-constructor :session?
+                                      (with (get (:query-params request) "_sw_session_p")
+                                        (if (nil? it)
+                                          true
+                                          (json-parse it))))
              (make-Application :rest-handler not-found-page-handler :session? false)))
     (reset! (:cookies @it) (:cookies request))))
 

src/symbolicweb/common.clj
@@ -149,7 +149,7 @@ APPLICATION and VIEWPORT are bound within BODY."
                                  :fit-fn fit-fn
                                  :cookie-name "_sw_application_id"
                                  :cookie-path cookie-path
-                                 :application-constructor-fn (fn [] (application-constructor-fn app-type-data))
+                                 :application-constructor-fn application-constructor-fn
                                  :agent (agent name)
                                  args))
     (swap! -application-types- #(assoc % name @app-type-data))))

commit 66558f5e316ab843984b24a9093cc801567297e8

Fix for broken compile after 8f7db2c9eb83bbf223cb18b16f7c90d991535812

src/symbolicweb/application.clj
@@ -66,6 +66,7 @@ On page load (or refresh), the order of things executed are:
           (recur (next app-types)))))))
 
 
+(declare json-parse)
 (defn find-or-create-application-instance [request]
   (with1 (if-let [cookie-value (:value (get (:cookies request) "_sw_application_id"))]
            ;; Session cookie sent.