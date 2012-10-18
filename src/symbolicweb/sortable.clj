841bc777f16cc1a844cfc4406e7de27c58d3be4e
Lars Rune Nøstdal <larsnostdal@gmail.com>
2012-10-18 23:36:52 +0100

    Sortable: Update this based on the changes to WidgetBase and ContainerView.

--- src/symbolicweb/sortable.clj
+++ src/symbolicweb/sortable.clj
@@ -2,46 +2,67 @@
 
 
 (defn sortable [^WidgetBase container-view ^ContainerModel container-model & callback]
-  (add-response-chunk (str "$('#" (.id container-view) "').sortable();") container-view)
-  (set-event-handler
-   "sortupdate" container-view
-   (fn [& {:keys [new-order]}]
-     (dosync
-      (println "sortable: retry?")
-      (let [^clojure.lang.PersistentVector
-            new-order-ids (json-parse new-order)
+  (letfn [(update-dom-hash-codes []
+            ;; Assign the hashCodes of the CMNs of CONTAINER-MODEL to the DOM so we can find the CMNs later.
+            (loop [node (cm-head-node container-model)
+                   index 1]
+              (add-response-chunk (str "$('#" (.id container-view) " *:nth-child(" index ")')"
+                                       ".data('cmn_hash_code', '" (.hashCode node) "');")
+                                  container-view)
+              (when-let [right-node (cmn-right-node node)]
+                (recur right-node
+                       (inc index)))))]
 
-            ^clojure.lang.PersistentVector
-            new-order-nodes (with-local-vars [nodes []]
-                              (doseq [widget-id new-order-ids]
-                                (var-alter nodes conj (:node-of-view @(:aux (get-widget widget-id (viewport-of container-view))))))
-                              (var-get nodes))
+    (add-response-chunk (str "$('#" (.id container-view) "').sortable();") container-view)
+    (update-dom-hash-codes)
 
-            ^clojure.lang.PersistentVector
-            existing-order-nodes (with-local-vars [nodes []]
-                                   (loop [node (cm-head-node container-model)]
-                                     (var-set nodes (conj (var-get nodes) node))
-                                     (when-let [right-node (cmn-right-node node)]
-                                       (recur right-node)))
-                                   (var-get nodes))]
+    (set-event-handler
+     "sortupdate" container-view
+     (fn [& {:keys [new-order]}]
+       (dosync
+        (println "sortable: retry?")
+        (let [^clojure.lang.PersistentVector
+              new-order-hash-codes (json-parse new-order)
 
-        ;; Do some simple checks to ensure the client doesn't have less (or different) nodes than the server as we start removing
-        ;; EXISTING-ORDER-NODES on the server end the adding (wrongly) only a few of them back via NEW-ORDER-NODES.
-        (assert (= (count new-order-nodes)
-                   (count existing-order-nodes)))
-        (doseq [new-node new-order-nodes]
-          (assert (not= -1 (.indexOf existing-order-nodes new-node))))
+              ^clojure.lang.PersistentVector
+              existing-order-nodes (with-local-vars [nodes []]
+                                     (loop [node (cm-head-node container-model)]
+                                       (var-alter nodes conj node)
+                                       (when-let [right-node (cmn-right-node node)]
+                                         (recur right-node)))
+                                     (var-get nodes))
 
-        ;; TODO: Quite crude redraw.
-        ;; This also fucks up stuff (uhm, what stuff?) that still thinks the old nodes are what holds our current view of auctions.
-        (doseq [node existing-order-nodes]
-          (cmn-remove node))
-        (time
-         (doseq [node new-order-nodes]
-           (cm-append container-model (cmn (cmn-data node)))))
+              ^clojure.lang.PersistentVector
+              new-order-nodes (with-local-vars [nodes []]
+                                (doseq [hash-code new-order-hash-codes]
+                                  (loop [node (cm-head-node container-model)]
+                                    (when (= hash-code (str (.hashCode node)))
+                                      (var-alter nodes conj node))
+                                    (when-let [right-node (cmn-right-node node)]
+                                      (recur right-node))))
+                                (var-get nodes))]
 
-        (when callback (callback new-order-nodes))))
-     (println "sortable: done!"))
-   :callback-data {:new-order (str "' + encodeURIComponent(JSON.stringify($('#"
-                                   (.id container-view)
-                                   "').sortable('toArray'))) + '")}))
+
+          ;; Do some simple checks to ensure the client doesn't have less (or different) nodes than the server as we start
+          ;; removing EXISTING-ORDER-NODES on the server end the adding (wrongly) only a few of them back via NEW-ORDER-NODES.
+          (assert (= (count new-order-nodes)
+                     (count existing-order-nodes)))
+          (doseq [new-node new-order-nodes]
+            (assert (not= -1 (.indexOf existing-order-nodes new-node))))
+
+          ;; TODO: Quite crude redraw. This also fucks up stuff (uhm, what stuff?) that still thinks the old nodes are what holds
+          ;; our current view of auctions.
+          (doseq [node existing-order-nodes]
+            (cmn-remove node))
+          (time
+           (doseq [node new-order-nodes]
+             (cm-append container-model (cmn (cmn-data node)))))
+
+          (update-dom-hash-codes)
+
+          (when callback (callback new-order-nodes))))
+       (println "sortable: done!"))
+     :callback-data {:new-order
+                     (str "' + encodeURIComponent(JSON.stringify(jQuery.makeArray($('#" (.id container-view) "')"
+                          ".children().map(function(index, element){ return($(element).data('cmn_hash_code')); })"
+                          "))) + '")})))
