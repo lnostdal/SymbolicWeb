ef48f3666b46cfb0eb2c2657226070b46fc2c4d4
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-03-11 11:59:31 +0000

    UserModelBase: Add :UUID field.

--- src/symbolicweb/user.clj
+++ src/symbolicweb/user.clj
@@ -1,9 +1,24 @@
 (in-ns 'symbolicweb.core)
 
 
+
+(defn user-model-base-clj-to-db-transformer [m]
+  "SW --> DB"
+  (-> m
+      ((fn [m]
+         (case (:key m)
+           :sessions
+           (assoc m
+             :key nil)
+
+           m)))))
+
+
+
 (defn mk-UserModelBase [& args]
   (ref (apply assoc {}
               :id (vm nil)
               ;; A single user can have several sessions running on several computers/browsers at the same time.
               :sessions (vm #{}) ;; NOTE: GC-SESSION depends on this field.
+              :uuid (generate-uuid)
               args)))
