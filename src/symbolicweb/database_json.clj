f31e31ab6758d7a62539316c7bfd87fc9040f6f9
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-03-27 07:36:06 +0000

    DB-JSON-GET: Handle "not found" case correctly and better.

--- src/symbolicweb/database_json.clj
+++ src/symbolicweb/database_json.clj
@@ -40,7 +40,7 @@
                                         (map #(vm %) (vals js-m))))}]
               ;; Observe the store itself; see the VM-ALTER call in DB-JSON-GET.
               (vm-observe (:data js) nil false (fn [_ _ _] (db-json-update js)))
-              ;; Observe each field in the store.
+              ;; Observe each existing field in the store.
               (doseq [[k v] @(:data js)]
                 (vm-observe v nil false (fn [_ _ _] (db-json-update js))))
               js)))))
@@ -53,19 +53,12 @@
 
 
 
-;; TODO: The NOT-FOUND stuff here means storing NIL doesn't make sense, and it seems "off" in general..
-(defn db-json-get
-  ([json-store ^Keyword k]
-     (db-json-get json-store k
-                  (with1 (vm nil)
-                    (let [added-to-store? (ref false)]
-                      (vm-observe it nil false
-                                  (fn [_ _ new-value]
-                                    (if (ensure added-to-store?)
-                                      (db-json-update json-store)
-                                      (do
-                                        ;; Triggers a DB-JSON-UPDATE via observer in DB-JSON-STORE-GET.
-                                        (vm-alter (:data json-store) assoc k it)
-                                        (ref-set added-to-store? true)))))))))
-  ([json-store ^Keyword k not-found]
-     (get @(:data json-store) k not-found)))
+(defn db-json-get [json-store ^Keyword k & not-found]
+  (let [res (get @(:data json-store) k ::not-found)]
+    (if (= res ::not-found)
+      (with1 (vm not-found)
+        ;; Observe new field in the store.
+        (vm-observe it nil false (fn [_ _ _] (db-json-update json-store)))
+        ;; Add field to store.
+        (vm-alter (:data json-store) assoc k it))
+      res)))
