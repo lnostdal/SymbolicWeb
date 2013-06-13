78e7489dfa2d212d11a8120f84f63f43d6baebcf
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-06-13 16:05:31 +0100

    HTMLContainer / WHC: More flexible handling of HTML attr. args.

--- src/symbolicweb/html_container.clj
+++ src/symbolicweb/html_container.clj
@@ -2,28 +2,38 @@
 
 
 
-(defn ^WidgetBase %mk-HTMLContainer [html-element-type ^Fn content-fn widget-base-args]
-  (let [html-element-type (name html-element-type)]
-    (mk-WidgetBase (fn [^WidgetBase html-container]
-                     (binding [*in-html-container?* html-container] ;; Target for calls to SW done in CONTENT-FN.
-                       (str "<" html-element-type " id='" (.id html-container) "'>"
-                            (content-fn html-container)
-                            "</" html-element-type ">")))
-                   (apply hash-map widget-base-args))))
+;; TODO: Code in mk-WB is very similar to this.
+(defn ^WidgetBase %mk-HTMLContainer [^Keyword html-element-type args ^Fn content-fn]
+  (mk-WidgetBase (fn [^WidgetBase html-container]
+                   (binding [*in-html-container?* html-container] ;; Target for calls to SW done in CONTENT-FN.
+                     (if (empty? args)
+                       (let [html-element-type (name html-element-type)]
+                         (str "<" html-element-type " id='" (.id html-container) "'>"
+                              (content-fn html-container)
+                              "</" html-element-type ">"))
+                       (html
+                        [html-element-type (let [attrs (:html-attrs args)]
+                                             (if-let [id (:id attrs)]
+                                               attrs
+                                               (assoc attrs :id (.id html-container))))
+                         (content-fn html-container)]))))
+                 (if-let [id (:id (:html-attrs args))]
+                   (assoc args :id id)
+                   args)))
 
 
 
-(defmacro whc [[html-element-type & widget-base-args] & body]
+(defmacro whc [[html-element-type args] & body]
   "WITH-HTML-CONTAINER."
   `(%mk-HTMLContainer ~html-element-type
-                      (fn [_#] ~@body)
-                      '~widget-base-args))
+                      ~args
+                      (fn [_#] ~@body)))
 
 
 
-(defn ^WidgetBase mk-PostHTMLTemplate [^String id ^Fn content-fn & widget-base-args]
+(defn ^WidgetBase mk-PostHTMLTemplate [^String id ^Fn content-fn & args]
   "This applies templating to an already existing HTML element, specified by ID, on the page."
-  (with1 (%mk-HTMLContainer "%PostHTMLTemplate"
-                            content-fn
-                            (into widget-base-args (list id :id))) ;; TODO: This seems hacky.
+  (with1 (%mk-HTMLContainer :%PostHTMLTemplate
+                            (into args (list id :id)) ;; TODO: This seems hacky.
+                            content-fn)
     (render-html it)))
