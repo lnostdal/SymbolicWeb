c60359e897110d2b09387f5184c13631663383df
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-07-17 15:40:07 +0100

    History: Do some very basic sanity checking to resolve some IE related issues (page reloads).

--- resources/js/sw-ajax.js
+++ resources/js/sw-ajax.js
@@ -7,12 +7,16 @@
 
 if(!window.history.replaceState) {
     window.history.replaceState = function(x, y, url_search){
-        window.location.replace("//" + window.location.host + window.location.pathname + url_search);
+        if(url_search != window.location.search){
+            window.location.replace("//" + window.location.host + window.location.pathname + url_search);
+        }
     }
 }
 if(!window.history.pushState){
     window.history.pushState = function(x, y, url_search){
-        window.location.assign("//" + window.location.host + window.location.pathname + url_search);
+        if(url_search != window.location.search){
+            window.location.assign("//" + window.location.host + window.location.pathname + url_search);
+        }
     }
 }
 
