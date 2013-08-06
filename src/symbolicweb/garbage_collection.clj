68d1128b0e84bd813f01bad3d59602d839029674
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-08-06 04:00:31 +0100

    GC: Do some work on issue #23.

--- src/symbolicweb/garbage_collection.clj
+++ src/symbolicweb/garbage_collection.clj
@@ -3,45 +3,41 @@
 
 
 (defn gc-viewport [^Ref viewport]
-  (dosync
-   (let [viewport-m (ensure viewport)]
-     ;; This will call the DO-LIFETIME-DEACTIVATION CBs which will disconnect the widgets from their models (Observables).
-     (detach-lifetime (.lifetime (:root-element viewport-m)))
-     ;; Session -/-> Viewport.
-     (alter (:viewports (ensure (:session viewport-m)))
-            dissoc (:id viewport-m)))))
+  (let [viewport-m (ensure viewport)]
+    ;; This will call the DO-LIFETIME-DEACTIVATION CBs which will disconnect the widgets from their models (Observables).
+    (detach-lifetime (.lifetime (:root-element viewport-m)))
+    ;; Session -/-> Viewport.
+    (alter (:viewports (ensure (:session viewport-m)))
+           dissoc (:id viewport-m))))
 
 
 
 (defn gc-session [^Ref session]
-  (dosync
-   (let [session-m (ensure session)]
-     (alter -sessions- dissoc (:uuid session-m))
-     (when-not (:one-shot? @session)
-       (with-sw-io nil
-         (swsync
-          (if (= "permanent" @(spget session :session-type))
-            (db-cache-remove (db-get-cache "sessions") @(:id session-m))
-            (db-remove session "sessions")))))
-     (vm-alter -num-sessions-model- - 1)
+  (let [session-m (ensure session)]
+    (alter -sessions- dissoc (:uuid session-m))
+    (when-not (:one-shot? @session)
+      (if (= "permanent" @(spget session :session-type))
+        (db-cache-remove (db-get-cache "sessions") @(:id session-m))
+        (db-remove session "sessions")))
+    (vm-alter -num-sessions-model- - 1)
 
-     ;; GC all Viewports in SESSION.
-     (doseq [[viewport-id viewport] (ensure (:viewports session-m))]
-       (gc-viewport viewport))
+    ;; GC all Viewports in SESSION.
+    (doseq [[viewport-id viewport] (ensure (:viewports session-m))]
+      (gc-viewport viewport))
 
-     ;; UserModel -/-> Session
-     (when-let [user-model @(:user-model session-m)]
-       (vm-alter (:sessions @user-model) disj session)))))
+    ;; UserModel -/-> Session
+    (when-let [user-model @(:user-model session-m)]
+      (vm-alter (:sessions @user-model) disj session))))
 
 
 
 (defn do-gc []
-  (let [now (System/currentTimeMillis)]
-    (doseq [[session-id session] @-sessions-]
-      (if (< -session-timeout- (- now @(:last-activity-time @session)))
-        (gc-session session)
-        ;; The Session hasn't timed out, but perhaps some of the Viewports in the Session has?
-        (dosync
+  (swsync
+   (let [now (System/currentTimeMillis)]
+     (doseq [[session-id session] (ensure -sessions-)]
+       (if (< -session-timeout- (- now @(:last-activity-time @session)))
+         (gc-session session)
+         ;; The Session hasn't timed out, but perhaps some of the Viewports in the Session has?
          (doseq [[viewport-id viewport] (ensure (:viewports @session))]
            (when (< -viewport-timeout- (- now @(:last-activity-time @viewport)))
              (gc-viewport viewport))))))))
