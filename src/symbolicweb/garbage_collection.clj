7da57096b6ee21e04daea65cccfc644b8a94dcea
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-08-22 22:59:08 +0100

    -NUM-SESSIONS-MODEL-: Should be (more?) correct now..

--- src/symbolicweb/garbage_collection.clj
+++ src/symbolicweb/garbage_collection.clj
@@ -18,8 +18,8 @@
     (when-not (:one-shot? @session)
       (if (= "permanent" @(spget session :session-type))
         (db-cache-remove (db-get-cache "sessions") @(:id session-m))
-        (db-remove session "sessions")))
-    (vm-alter -num-sessions-model- - 1)
+        (db-remove session "sessions"))
+      (vm-alter -num-sessions-model- - 1))
 
     ;; GC all Viewports in SESSION.
     (doseq [[viewport-id viewport] (ensure (:viewports session-m))]
