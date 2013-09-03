1333b4899c925e02f4b4560709af1b05cc85212b
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-09-03 01:54:52 +0100

    Mail stuff.

--- src/symbolicweb/mail_chimp.clj
+++ src/symbolicweb/mail_chimp.clj
@@ -14,35 +14,77 @@
 
 
 
-(defn mk-MailChimp [api-key]
-  {:api-key api-key
-   :url "https://mandrillapp.com/api/1.0/"})
+;;;; Mail Chimp: Base
+
+(defn mk-MailChimp [^String api-key]
+  {:url (str "https://" (second (.split api-key "-")) ".api.mailchimp.com/2.0/")
+   :api-key api-key})
+
+
+
+(defn mailchimp-call [context ^String cmd m]
+  )
+
+
+
+(defn mailchimp-batch-subscribe [context ^String list-id m]
+  (http-post-request (str (:url context) "lists/batch-subscribe.json")
+                     (json-generate (merge {:apikey (:api-key context)
+                                            :id list-id
+                                            :double_optin false
+                                            :update_existing true}
+                                           m))))
+
+
+(defn mailchimp-batch-unsubscribe [context ^String list-id m]
+  (http-post-request (str (:url context) "lists/batch-unsubscribe.json")
+                     (json-generate (merge {:apikey (:api-key context)
+                                            :id list-id
+                                            :send_goodbye false
+                                            :send_notify false}
+                                           m))))
+
+
+(defn mailchimp-subscribe [context ^String list-id m]
+  (http-post-request (str (:url context) "lists/subscribe.json")
+                     (json-generate (merge {:apikey (:api-key context)
+                                            :id list-id
+                                            :double_optin false
+                                            :update_existing true}
+                                           m))))
+
+#_(swsync
+ (swmail/mailchimp-subscribe (swmail/mk-MailChimp "4d58a3cb5dd97dd3940787f1b63a85f2-us5")
+                             "333317793c"
+                             {:email {:email "larsnostdal@gmail.com" :email_type "html"}}))
+
+
+(defn mailchimp-unsubscribe [context ^String list-id m]
+  (http-post-request (str (:url context) "lists/unsubscribe.json")
+                     (json-generate (merge {:apikey (:api-key context)
+                                            :id list-id
+                                            :send_goodbye false
+                                            :send_notify false}
+                                           m))))
+
+
+
 
 
 ;;;; Mail Chimp: Mandrill
 ;;;; http://mandrillapp.com/api/docs/index.html
 
 
+(defn mk-Mandrill [^String api-key]
+  {:api-key api-key
+   :url "https://mandrillapp.com/api/1.0/"})
 
-;;; Users Calls
-
-(defn users-info [context]
-  (http-post-request (str (:url context) "users/info.json")
-                     (json-generate {:key (:api-key context)})))
-
-(defn users-ping [context]
-  (http-post-request (str (:url context) "users/ping.json")
-                     (json-generate {:key (:api-key context)})))
-
-(defn users-senders [context]
-  (http-post-request (str (:url context) "users/senders.json")
-                     (json-generate {:key (:api-key context)})))
 
 
 
 ;;; Messages Calls
 
-(defn messages-send [context message]
+(defn mandrill-messages-send [context message]
   (http-post-request (str (:url context) "messages/send.json")
                      (json-generate {:key (:api-key context)
                                      :message message})))
