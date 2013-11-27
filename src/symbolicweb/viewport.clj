2d35d575d936ed016501e41bf1b62dcd925fdff7
Lars Rune Nøstdal <larsnostdal@gmail.com>
2013-11-27 02:40:28 +0000

    Various..

--- src/symbolicweb/viewport.clj
+++ src/symbolicweb/viewport.clj
@@ -58,10 +58,10 @@
                                 :callback-data {:query-string "' + encodeURIComponent(window.location.search.slice(1)) + '"})
 
     ;; Viewport scrolled to the bottom
-    (set-viewport-event-handler "window" "sw_scrollbottom" viewport
+    #_(set-viewport-event-handler "window" "sw_scrollbottom" viewport
                                 (fn [& _] (vm-alter (:scrolled-to-bottom-event @viewport) inc)))
-    ;; TODO: Reduce spammy event rate.
-    (add-response-chunk
+    ;; TODO: Reduce spammy event rate: http://underscorejs.org/#throttle
+    #_(add-response-chunk
      (str "$(window).scroll(function(){"
           "  if(50 > $(document).height() - $(window).height() - $(window).scrollTop()){"
           "    $(window).trigger('sw_scrollbottom');"
