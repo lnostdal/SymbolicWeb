72a6d74f8f7280f30cc2e6abded65797cb057835
Lars Rune Nøstdal <larsnostdal@gmail.com>
2014-03-06 14:09:08 +0100

    Foundation and Bootstrap bare bone examples added.

---
+++ src/symbolicweb/examples/bootstrap.clj
@@ -0,0 +1,41 @@
+(ns symbolicweb.examples.bootstrap
+  (:import clojure.lang.Ref
+           clojure.lang.Keyword
+           clojure.lang.Fn)
+  (:use symbolicweb.core))
+
+
+
+(defmethod symbolicweb.examples.nostdal-org/mk-nostdal-org-viewport "/bootstrap" [^String uri request session]
+  (let [root-widget (mk-bte :id "_body" :root-widget? true)
+        viewport (mk-Viewport request session root-widget :page-title "SW: Bootstrap")]
+    (add-rest-head viewport "<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />")
+
+    (add-resource viewport :css "bootstrap/dist/css/bootstrap.min.css")
+    (add-resource viewport :css "bootstrap/dist/css/bootstrap-theme.min.css")
+    (add-resource viewport :js "bootstrap/dist/js/bootstrap.min.js")
+
+    (jqAppend root-widget
+      (whc [:div {:class "container-fluid"}]
+
+        [:nav {:class "navbar navbar-default" :role "navigation"}
+         [:div {:class "container-fluid"}
+          [:div {:class "navbar-header"}
+           [:button {:type "button" :class "navbar-toggle" :data-toggle "collapse" :data-target ".navbar-collapse"}
+            [:span {:class "sr-only"} "Toggle navigation"]
+            [:span {:class "icon-bar"}] [:span {:class "icon-bar"}] [:span {:class "icon-bar"}]]
+           [:a {:class "navbar-brand" :href "#"} "nostdal.org"]]
+
+          [:div {:class "collapse navbar-collapse"}
+           [:ul {:class "nav navbar-nav"}
+            [:li [:a {:href "#"} "First button"]]
+            [:li [:a {:href "#"} "Second button"]]]]]]
+
+
+         [:h2 "SymbolicWeb"]
+
+         [:h3 "Bootstrap: basic example"]
+         [:p "This example shows how to get " [:a {:href "http://getbootstrap.com/"} "Bootstrap"]
+         " up and running with SymbolicWeb."]))
+
+    viewport))
---
+++ src/symbolicweb/examples/foundation.clj
@@ -0,0 +1,45 @@
+(ns symbolicweb.examples.foundation
+  (:import clojure.lang.Ref
+           clojure.lang.Keyword
+           clojure.lang.Fn)
+
+  (:use symbolicweb.core))
+
+
+
+(defmethod symbolicweb.examples.nostdal-org/mk-nostdal-org-viewport "/foundation" [^String uri request session]
+  (let [root-widget (mk-bte :id "_body" :root-widget? true)
+        viewport (mk-Viewport request session root-widget :page-title "SW: Foundation")]
+    (add-rest-head viewport "<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />")
+    (add-resource viewport :css "foundation/dist/assets/css/normalize.css")
+    (add-resource viewport :css "foundation/dist/assets/css/foundation.min.css")
+    (add-resource viewport :js "Modernizr/dist/modernizr-build.min.js")
+    (add-resource viewport :js "foundation/dist/assets/js/foundation.min.js")
+
+    (jqAppend root-widget
+      (whc [:div]
+        [:nav {:class "top-bar" :data-topbar "" :data-options "is_hover: false"}
+
+         [:ul {:class "title-area"}
+          [:li {:class "name"}
+           [:h1 [:a "nostdal.org"]]]
+          [:li {:class "toggle-topbar menu-icon"}
+           [:a "Menu"]]]
+
+         [:section {:class "top-bar-section"}
+          [:ul {:class "left"}
+           [:li [:a "Left Nav Button"]]]
+
+
+          [:ul {:class "right"}
+           [:li [:a "Right Nav Button"]]]]]
+
+        [:h2 "SymbolicWeb"]
+        [:h3 "Foundation: basic example"]
+        [:p "This example shows how to get " [:a {:href "http://foundation.zurb.com/"} "Foundation"]
+         " up and running with SymbolicWeb."]
+
+
+        [:script "$(document).foundation();"]))
+
+    viewport))
