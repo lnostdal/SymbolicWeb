c4565c6992824f1cffd892828f7b161053014b56
Lars Rune Nøstdal <larsnostdal@gmail.com>
2014-09-12 16:10:36 +0200

    Chat example: Make it a little nicer.

--- src/symbolicweb/examples/chat.clj
+++ src/symbolicweb/examples/chat.clj
@@ -13,33 +13,47 @@
                      (when-not @it
                        (vm-set it (str "anon-" (Long/toHexString (generate-uid)))))
                      it)
-          conversation-area-view (mk-ContainerView (whc [:div])
-                                                   conversation-area-model
-                                                   (fn [container-view node]
-                                                     (let [[nickname-model msg] (cmn-data node)]
-                                                       (whc [:div]
-                                                         [:b (sw (mk-span nickname-model))] ": "
-                                                         (escape-html msg)))))]
+          conversation-area-view
+          (mk-ContainerView (whc [:div {:style "overflow: auto;"}])
+                            conversation-area-model
+                            (fn [container-view node]
+                              (let [[nickname-model msg] (cmn-data node)]
+                                (whc [:div]
+                                  [:b (sw (mk-span nickname-model))] ": " (escape-html msg)
+                                  ;; Scroll consversation to most recent message.
+                                  [:script "$('#" (.id container-view) "')"
+                                   ".scrollTop($('#" (.id container-view) "')"
+                                   ".prop('scrollHeight'));"]))))]
+
       (jqAppend root-widget
         (whc [:div]
-          [:div (sw conversation-area-view)]
-          [:table {:style "width: 100%;"}
-           [:tr
-            [:td {:style "white-space: nowrap;"} "Nick: " (sw (mk-TextInput nickname :change)) " | "]
-            [:td {:style "width: 100%;"}
-             (let [chat-input-model (vm "")
-                   chat-input-view (with1 (mk-TextInput chat-input-model :enterpress
-                                                        :clear-on-submit? true)
-                                     (jqCSS it "width" "100%"))]
-               (js-run chat-input-view "$('#" (.id chat-input-view) "').focus();")
-               (vm-observe chat-input-model nil false
-                           (fn [inner-lifetime old-value new-value]
-                             (when new-value
-                               (cm-append conversation-area-model (cmn [nickname new-value])))))
-               (sw chat-input-view))]]]
+          (sw conversation-area-view)
+          [:div {:id "chat_input"}
+           [:table {:border 0 :style "width: 100%;"}
+            [:tr
+             [:td {:style "white-space: nowrap;"}
+              "Nick: " (sw (with1 (mk-TextInput nickname :change)
+                             (jqCSS it "width" "5em")))
+              " | "]
+             [:td {:style "width: 100%;"}
+              (let [chat-input-model (vm "")
+                    chat-input-view (with1 (mk-TextInput chat-input-model :enterpress :clear-on-submit? true)
+                                      (jqCSS it "width" "100%"))]
+                (js-run chat-input-view "$('#" (.id chat-input-view) "').focus();")
+                (vm-observe chat-input-model nil false
+                            (fn [inner-lifetime old-value new-value]
+                              (when new-value
+                                (cm-append conversation-area-model (cmn [nickname new-value])))))
+                (sw chat-input-view))]]]]
 
-          [:hr]
-          [:pre
+          [:script
+           "$(window).resize(function(){"
+           "  $('#" (.id conversation-area-view) "').outerHeight($(window).innerHeight() - $('#chat_input').outerHeight() - 30);"
+           "});"
+           "$(window).trigger('resize');"]
+
+          #_[:hr]
+          #_[:pre
            [:a {:href "https://github.com/lnostdal/SymbolicWeb/blob/master/src/symbolicweb/examples/chat.clj"}
             "Source code"]]))
 
